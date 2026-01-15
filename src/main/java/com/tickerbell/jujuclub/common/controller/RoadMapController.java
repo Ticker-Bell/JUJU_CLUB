1b003533c9c4fd551295a20928c211f6c3fef850
uk10105 <lykalso33@naver.com>
2026-01-15 12:34:49 +0900

    fix: MainController 제거 및 탭별 Controller 생성 #15

--- src/main/java/com/tickerbell/jujuclub/common/controller/RoadMapController.java
+++ src/main/java/com/tickerbell/jujuclub/common/controller/RoadMapController.java
@@ -1,4 +1,19 @@
 package com.tickerbell.jujuclub.common.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/roadMap")
 public class RoadMapController {
+
+    @GetMapping("/main.do")
+    public String roadMap(Model model) {
+
+        model.addAttribute("test","테스트");
+        return "roadMap/roadMapMain";
+    }
+
 }

59f9ca8dfb6d77d8e33e1197cee0253edb0c6f99
mJh0201 <ggb05091@naver.com>
2026-01-16 16:00:54 +0900

    bug:투자 마이페이지 기능 ë¸버그 픽스후 재업로드 #69

--- src/main/webapp/resources/js/main/home.js
+++ src/main/webapp/resources/js/main/home.js
@@ -439,7 +439,7 @@ document.addEventListener("DOMContentLoaded", () => {
             btnSubmitSurvey.innerText = originalBtnText;
         }
     }
-    //주석2
+    //주석22
     function goToFinalStep() {
         formAccountSuccess.style.opacity = "0";
         formAccountSuccess.style.transform = "translateY(10px)";

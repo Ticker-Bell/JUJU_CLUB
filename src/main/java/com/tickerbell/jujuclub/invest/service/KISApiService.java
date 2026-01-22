c26e6375236e70f87f72d702059526e0958f6a2f
goodoong <yunsun4830@gmail.com>
2026-01-22 18:37:46 +0900

    refactor: access 토큰 발급 자동화 로직 추가 #126
    - javax servlet api 버전 수정
    - 토큰을 applicaiton scope에 저장하여 사용하는 모든 유저에 동일 값 적용
    - 실제 api에 발급 로직, 토큰 확인 로직 분리

--- src/main/java/com/tickerbell/jujuclub/invest/service/KISApiService.java
+++ src/main/java/com/tickerbell/jujuclub/invest/service/KISApiService.java
@@ -1,6 +1,9 @@
 package com.tickerbell.jujuclub.invest.service;
 
 import com.tickerbell.jujuclub.invest.dto.KISDataDTO;
+import com.tickerbell.jujuclub.utils.GetValidAccessToken;
+import com.tickerbell.jujuclub.utils.RequestNewAccessToken;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,10 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@RequiredArgsConstructor
 @Service
 public class KISApiService {
+    private final RequestNewAccessToken requestNewAccessToken;
 
     @Value("${kis.appkey}")
     private String APP_KEY;
@@ -22,81 +27,6 @@ public class KISApiService {
     @Value("${kis.baseurl}")
     private String BASE_URL;
 
-    //토큰은 재사용하기
-    private String accessToken = null;
-    private long tokenExpireTime = 0; //만료시간(토큰 발급시 같이 들어옴)
-
-    public String getAccessToken() throws Exception {
-
-        //토근이 발급됐다면 다시 사용
-        if (accessToken != null && System.currentTimeMillis() < tokenExpireTime - 60000) {
-            return accessToken;
-        }
-
-        HttpURLConnection conn = null;
-
-        try{
-            //토큰 발급 요청 보낼 주소
-            URL url = new URL(BASE_URL + "/oauth2/tokenP");
-
-            //연결
-            conn = (HttpURLConnection) url.openConnection();
-
-            //데이터보내는방식
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
-
-            //보낼 데이터
-            String requestBody = "{"
-                    + "\"grant_type\": \"client_credentials\","
-                    + "\"appkey\": \"" + APP_KEY + "\","
-                    + "\"appsecret\": \"" + APP_SECRET + "\""
-                    + "}";
-
-            //데이터 전송
-            try(OutputStream os = conn.getOutputStream()){
-                os.write(requestBody.getBytes("UTF-8"));
-            }
-
-            //응답 받기
-            StringBuilder response = new StringBuilder();
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"))) {
-                String line;
-                while ((line = br.readLine()) != null){
-                    response.append(line);
-                }
-            }
-
-            //토큰 추출
-            String jsonResponse = response.toString();
-            int startIndex = jsonResponse.indexOf("\"access_token\":\"") + 16;
-            int endIndex = jsonResponse.indexOf("\"", startIndex);
-            accessToken = jsonResponse.substring(startIndex, endIndex);
-            //토큰 만료시간 가져오기
-            int expiresKey = jsonResponse.indexOf("\"expires_in\":");
-            if (expiresKey != -1) {
-                int startExpires = expiresKey + 13; //expires_in:길이13칸
-                int endExpires = jsonResponse.indexOf(",", startExpires);
-                if (endExpires == -1) {
-                    endExpires = jsonResponse.indexOf("}", startExpires);
-                }
-                //시간 추출
-                String expiresTimeStr = jsonResponse.substring(startExpires, endExpires).trim();
-                long expiresTime = Long.parseLong(expiresTimeStr);//사용가능시간
-                tokenExpireTime = System.currentTimeMillis() + (expiresTime * 1000); //만료시간
-
-                System.out.println("토큰발급완료");
-            } else {
-                tokenExpireTime = System.currentTimeMillis() + (86400 * 1000); //못찾을경우 기본값 처리
-            }
-            return accessToken;
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-    }
 
     //현재가 조회 : 토큰 준비, KIS API 호출(토큰 + 종목코드), 응답에서 추출
     //등락률
@@ -107,7 +37,7 @@ public class KISApiService {
 
         try {
             //토큰가져오기
-            String token = getAccessToken();
+            String token = requestNewAccessToken.getAccessToken();
 
             //URL만들기(REST 주식현재가 시세[v1_국내주식-008] 사용)
             String urlStr = BASE_URL
---
+++ src/main/java/com/tickerbell/jujuclub/utils/GetValidAccessToken.java
@@ -0,0 +1,37 @@
+package com.tickerbell.jujuclub.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class GetValidAccessToken {
+    private RequestNewAccessToken requestNewToken;
+
+    public String getValidToken(HttpServletRequest request) throws Exception {
+        ServletContext application;
+        application = request.getServletContext();
+
+        String token = (String) application.getAttribute("API_TOKEN");
+        LocalDateTime expireAt =
+                (LocalDateTime) application.getAttribute("API_TOKEN_EXPIRE_AT");
+
+        // 토큰이 없거나 만료된 경우
+        if (token == null || expireAt == null || expireAt.isBefore(LocalDateTime.now())) {
+            token = requestNewToken.getAccessToken();
+
+            application.setAttribute("API_TOKEN", token);
+            application.setAttribute(
+                    "API_TOKEN_EXPIRE_AT",
+                    LocalDateTime.now().plusHours(24)
+            );
+        }
+
+        return token;
+    }
+
+}

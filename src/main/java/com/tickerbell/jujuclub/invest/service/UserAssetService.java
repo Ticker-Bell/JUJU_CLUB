804c7163a9737ed3211b5186a270beccfbff114d
mJh0201 <ggb05091@naver.com>
2026-01-27 12:14:23 +0900

    fix 유저 자산 로직 수정중

--- src/main/java/com/tickerbell/jujuclub/invest/service/UserAssetService.java
+++ src/main/java/com/tickerbell/jujuclub/invest/service/UserAssetService.java
@@ -5,6 +5,7 @@ import com.tickerbell.jujuclub.invest.dto.UserInvestSummeryDTO;
 import com.tickerbell.jujuclub.invest.mapper.UserAssetMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,48 +13,51 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserAssetService {
 
-    //총 평가 자산(보유 현금 + 투자금)(변함)
-    //보유 현금(USER_ACCOUNT.cash_balance)
-    //투자금(주식평가금액) = 현재가 * 보유수량(변함)
-    //수익률 = (( 총 평가자산 - 초기자산(온보딩 후 받는 자산) ) / 초기자산) * 100(변함)
-
     private final UserAssetMapper userAssetMapper;
     private final PortfolioService portfolioService;
     private static final long initial_cash = 1000000L; //초기자산 100만원
 
+    @Transactional(readOnly = true)
     public UserInvestSummeryDTO getUserInvestSummary(int userSeq){
 
-        //현금(DB에 값 들어있음)
+        //1.예수금(cash_balance)
         Long cashBalance = userAssetMapper.selectCashBalance(userSeq);
         if(cashBalance == null){
             cashBalance = 0L;
         }
-        //포트폴리오 - currentPrice
-        List<PortfolioAllocationItemDTO> items = portfolioService.getPortfolioAllocationItems(userSeq);
 
-        long totalUserAsset = 0L; //총 평가자산 = 보유 현금 + (현재가 * 수량)
-        //주식 평가금액 계산
+        //2.주식평가금액
         long totalStockValue = 0L;
-        for (PortfolioAllocationItemDTO data : items) {
-            totalStockValue += (long)data.getCurrentPrice() * data.getQuantity();
+
+        List<PortfolioAllocationItemDTO> portfolioList = portfolioService.getPortfolioAllocationItems(userSeq);
+
+        for(PortfolioAllocationItemDTO data : portfolioList){
+            totalStockValue += (long)data.getQuantity() * data.getCurrentPrice();
         }
 
-        //총 평가자산 계산
-        totalUserAsset = cashBalance + totalStockValue;
+        //3.총 평가자산
+        long totalAsset = cashBalance + totalStockValue;
 
-        //수익률 계산
-        double totalReturnPct = 0.0;
-        if (initial_cash > 0) {
-            //
-            totalReturnPct = ( (double)( totalUserAsset - initial_cash ) / initial_cash ) * 100;
+        //4.원금(초기자금 + 보상금)
+//        Long totalReward = userAssetMapper.selectChapTestReward(userSeq) + userAssetMapper.selectMissionReward(userSeq);
+        Long totalReward = userAssetMapper.selectUserTotalReward(userSeq);
+        if(totalReward == null){
+            totalReward = 0L;
+        }
+        long totalPrincipal = initial_cash + totalReward;
+
+        //5.총 수익률
+        long totalProfit = totalAsset - totalPrincipal; //총 수익금
+        double totalReturnProfit = 0.0;
+        if(totalPrincipal > 0){
+            totalReturnProfit = ((double) totalProfit / totalPrincipal) * 100;
         }
 
-        //DTO만들어서 return
         return UserInvestSummeryDTO.builder()
-                .totalAsset(totalUserAsset)
-                .cashBalance(cashBalance)
-                .totalStockValue(totalStockValue)
-                .totalReturnPct(totalReturnPct)
+                .totalAsset(totalAsset) //총 평가자산
+                .cashBalance(cashBalance) //보유현금
+                .totalStockValue(totalStockValue) //총 평가자산
+                .totalReturnPct(totalReturnProfit) //총 수익률
                 .build();
 
     }
